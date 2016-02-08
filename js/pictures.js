4e818adc728f01268fe2b0aa4d2fcbed66c9b2b6
spololin <spololin@gmail.com>
2016-02-08 20:37:55 +0300

    edit error JSON
    Исправил переменные и параметр запроса в getPictures()

--- js/pictures.js
+++ js/pictures.js
@@ -76,11 +76,10 @@
   function getPictures() {
     container.classList.add('pictures-loading');
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://o0.github.io/assets/json/pictures.json', true);
+    xhr.open('GET', '//o0.github.io/assets/json/pictures.json', true);
     xhr.timeout = 10000;
     xhr.onload = function(evt) {
-      var loadedPictures = JSON.parse(evt.srcElement.response);
-      pictures = loadedPictures;
+      pictures = JSON.parse(evt.srcElement.response);
       renderPictures(0);
       setActiveFilter(activeFilter, true);
       container.classList.remove('pictures-loading');
