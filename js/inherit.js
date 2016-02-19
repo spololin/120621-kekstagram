332c680b2b6b2261a10508afe038df06ecdd8d59
spololin <spololin@gmail.com>
2016-02-19 17:50:48 +0300

    conflict inherit

--- js/inherit.js
+++ js/inherit.js
@@ -6,7 +6,7 @@
    *
    * @param {function} child constructor function
    * @param {function} parent constructor function
-   */
+     */
   function inherit(child, parent) {
 
     var TempConstructor = function() {};
