81cde2eacd1732941979115f80788e36876c3cad
spololin <spololin@gmail.com>
2016-02-23 11:35:04 +0300

    add event click on likes
    Добавил обработчик нажатия на лайки

--- js/gallery.js
+++ js/gallery.js
@@ -7,12 +7,14 @@
     this.closeButton = document.querySelector('.gallery-overlay-close');
     this.photo = document.querySelector('.gallery-overlay-image');
     this.likes = document.querySelector('.gallery-overlay-controls-like');
+    this.likesCount = document.querySelector('.likes-count');
     this.comments = document.querySelector('.gallery-overlay-controls-comments');
     this.pictures = [];
     this.currentPicture = 0;
     this._onPhotoClick = this._onPhotoClick.bind(this);
     this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this);
     this._onCloseClick = this._onCloseClick.bind(this);
+    this._onSetLike = this._onSetLike.bind(this);
   }
 
   Gallery.prototype.render = function() {
@@ -24,13 +26,15 @@
     this.element.classList.remove('invisible');
     this.closeButton.addEventListener('click', this._onCloseClick);
     this.photo.addEventListener('click', this._onPhotoClick);
+    this.likes.addEventListener('click', this._onSetLike);
     window.addEventListener('keydown', this._onDocumentKeyDown);
   };
 
   Gallery.prototype.hide = function() {
     this.element.classList.add('invisible');
     this.closeButton.removeEventListener('click', this._onCloseClick);
-    this.photo.addEventListener('click', this._onPhotoClick);
+    this.photo.removeEventListener('click', this._onPhotoClick);
+    this.likes.removeEventListener('click', this._onSetLike);
     window.removeEventListener('keydown', this._onDocumentKeyDown);
   };
 
@@ -91,6 +95,21 @@
   Gallery.prototype.setPrevPictureIndex = function() {
     if (this.pictures[this.currentPicture - 1]) {
       this.currentPicture--;
+    }
+  };
+
+  Gallery.prototype._onSetLike = function() {
+    var currentObject = this.pictures[this.currentPicture];
+    if (currentObject.setLike !== true) {
+      currentObject.likes = currentObject.likes + 1;
+      this.likesCount.classList.add('likes-count-liked');
+      this.likesCount.innerHTML = currentObject.likes;
+      currentObject.setLike = true;
+    } else {
+      currentObject.likes = currentObject.likes - 1;
+      this.likesCount.classList.remove('likes-count-liked');
+      this.likesCount.innerHTML = currentObject.likes;
+      currentObject.setLike = false;
     }
   };
 
