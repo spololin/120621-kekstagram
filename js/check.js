471489e7bef09edd2c98d4f0c36d5a2486897e95
spololin <spololin@gmail.com>
2016-02-24 14:00:08 +0300

    opimize function
    Оптимизация функции определения типа фото

--- js/check.js
+++ js/check.js
@@ -1,29 +1,33 @@
-function getMessage(a, b) {
-  if (typeof(a) === "boolean") {
-    if (a) {
-      return "Переданное GIF-изображение анимировано и содержит " + b + " кадров"}
-    else {
-      return "Переданное GIF-изображение не анимировано";
+'use strict';
+
+(function() {
+  function getMessage(a, b) {
+    if (typeof(a) === 'boolean') {
+      if (a) {
+        return "Переданное GIF-изображение анимировано и содержит " + b + " кадров"}
+      else {
+        return "Переданное GIF-изображение не анимировано";
+      }
+    }
+
+    if (typeof(a) === 'number') {
+      return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов";
+    }
+
+    if(typeof(a) === 'object') {
+      if (typeof(b) !== 'object') {
+        var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+          sum += a[i];
+        }
+        return "Количество красных точек во всех строчках изображения: " + sum;
+      } else {
+        var square = 0;
+        for (i = 0; i < a.length; i++) {
+          square += a[i] * b[i];
+        }
+        return "Общая площадь артефактов сжатия: " + square + " пикселей";
+      }
     }
   }
-
-  if (typeof(a) === "number") {
-    return "Переданное SVG-изображение содержит " + a + " объектов и " + b * 4 + " аттрибутов";
-  }
-
-  if (typeof(a) === "object" && typeof(b) !== "object") {
-    var sum = 0;
-    for (var i = 0; i < a.length; i++) {
-      sum += a[i];
-    }
-    return "Количество красных точек во всех строчках изображения: " + sum;
-  }
-
-  if (typeof(a) === "object" && typeof(b) === "object") {
-    var square = 0;
-    for (i = 0; i < a.length; i++) {
-      square += a[i] * b[i];
-    }
-    return "Общая площадь артефактов сжатия: " + square + " пикселей";
-  }
-}
+})();
